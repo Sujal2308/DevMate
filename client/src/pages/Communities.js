a22f8000c5fcca4d7d76b91be568dd0aa834ad72
Sujal Bhugul <sujalbhugul08@gmail.com>
2026-05-15 00:42:32 +0530

    feat: implement Communities page with search, join, and leave functionality

--- client/src/pages/Communities.js
+++ client/src/pages/Communities.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "../config/axios";
-import { motion } from "framer-motion";
 
 const Communities = () => {
   const [communities, setCommunities] = useState([]);
